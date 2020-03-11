e4ce3eba310b4979d3c944f8c25646623ddce9ec
COTEKieran <60655662+COTEKieran@users.noreply.github.com>
2020-03-11 02:33:06 +0000

    Created server, linked htmlto JSON file, began login front-end

---
+++ form.js
@@ -0,0 +1,77 @@
+async function pageLoaded() {
+  const response = await fetch('questions.json');
+  const data = await response.json();
+  console.log(data);
+
+  read(data);
+}
+
+function read(data) {
+  console.log(data);
+  const body = document.getElementById('questions');
+  for (let i = 0; i < data.questions.length; i++) {
+    const questionName = document.createElement("h5");
+    questionName.textContent = data.questions[i].text;
+    body.appendChild(questionName);
+
+    if (data.questions[i].type == 'text') {
+      const txt = document.createElement('input');
+      txt.setAttribute('type', 'text');
+      body.appendChild(txt);
+    }
+
+    if (data.questions[i].type == 'number') {
+      const num = document.createElement('input')
+      num.setAttribute('type', 'number');
+      body.appendChild(num);
+    }
+
+    if (data.questions[i].type == 'single-select') {
+        const options = data.questions[i].options;
+        const ul = document.createElement('ul');
+        const length = data.questions[i].options.length;
+        for (let i = 0; i < length; i++) {
+          const li = document.createElement('li');
+          const single = document.createElement('input');
+          single.setAttribute('type', 'radio');
+          single.setAttribute('name', 'single');
+          const label = document.createElement('label');
+          label.textContent = options[i];
+          li.append(single);
+          li.append(label);
+          ul.appendChild(li);
+        }
+        body.appendChild(ul);
+      }
+
+
+      if (data.questions[i].type === 'multi-select') {
+        const options = data.questions[i].options;
+        const ul = document.createElement('ul');
+        const length = data.questions[i].options.length;
+        for (let i = 0; i < length; i++) {
+          const li = document.createElement('li');
+          const multiple = document.createElement('input');
+          multiple.setAttribute('type', 'checkbox');
+          multiple.setAttribute('name', 'mutiple');
+          const label = document.createElement('label');
+          label.textContent = options[i];
+          li.append(multiple);
+          li.append(label);
+          ul.appendChild(li);
+
+        }
+        body.appendChild(ul);
+
+      }
+
+    }
+  }
+
+    function dropdownFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onload = function () {
+      pageLoaded();
+    }
---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+
+app.use(express.static(__dirname));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.listen(8080,(err) => {
+  if (err) console.error('error encountered starting server', err);
+  else console.log('server has started');
+});
+
+
+
+app.post('/post-test', (req,res) => {
+  console.log('Got body:',req.body);
+  res.sendStatus(200);
+
+  fs.writeFile('pageText.txt', JSON.stringify(req.body), (err) => {
+    if (err) conole.log (err);
+    console.log("Successfully Written to File.");
+  });
+});
+
+
+function error(res,msg){
+  res.sendStatus(500);
+  console.error(msg);
+}
