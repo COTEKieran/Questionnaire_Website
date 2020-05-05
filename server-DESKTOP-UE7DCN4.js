367720f62723a5b4cd565a5e849a087dc6dc410f
COTEKieran <60655662+COTEKieran@users.noreply.github.com>
2020-05-05 15:08:38 +0100

    j

---
+++ server-DESKTOP-UE7DCN4.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+
+app.use(express.static(__dirname));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.listen(8080,(err) => {
+  if (err) console.error('error encountered starting server', err);
+  else console.log('server has started on localhost:8080');
+});
+
+
+
+app.post('/post-test', (req,res) => {
+  console.log('Got body:',req.body);
+  res.sendStatus(200);
+
+  fs.writeFile('pageText.txt', JSON.stringify(req.body), (err) => {
+    if (err) conole.log (err);
+    console.log("successfully Written to File.");
+  });
+});
+
+
+
+
+function error(res,msg){
+  res.sendStatus(500);
+  console.error(msg);
+}
