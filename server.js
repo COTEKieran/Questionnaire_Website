02a85bdfab07ed6f98e7fc8dc2d44fb12940975c
Kieran Blowes <kieranblowes10@gmail.com>
2020-05-23 17:58:04 +0100

    Finished OAuth login

--- server.js
+++ server.js
@@ -26,7 +26,19 @@ app.post('/post-test', (req,res) => {
   });
 });
 
-
+const GoogleAuth = require('simple-google-openid');
+ 
+// you can put your client ID here
+app.use(GoogleAuth('313462441845-tg48640feaon7g0pim1smph798l02lch.apps.googleusercontent.com'));
+ 
+// return 'Not authorized' if we don't have a user
+app.use('/api', GoogleAuth.guardMiddleware());
+ 
+app.get('/api/hello', (req, res) => {
+  res.send('Hello ' + (req.user.displayName || 'user without a name') + '!');
+ 
+  console.log('successful authenticated request by ' + req.user.emails[0].value);
+});
 
 
 function error(res,msg){
